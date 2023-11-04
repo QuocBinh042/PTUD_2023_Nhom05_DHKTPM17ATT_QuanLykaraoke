412a5c7e88c669ee291395d48bc7d6649d46e7dd
QuocBinh042 <tranlequocbinh2003@gmail.com>
2023-11-04 10:48:37 +0700

    Update HoaDon

--- Nhom05_QuanLyKaraoke/src/app/HoaDon.java
+++ Nhom05_QuanLyKaraoke/src/app/HoaDon.java
@@ -3,6 +3,7 @@ package app;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -26,77 +27,61 @@ public class HoaDon extends JPanel {
 			"Tên khách hàng", "Số điện thoại khách", "Tổng hoá đơn" };
 	private JLabel lblTenNV, lblSđtKH, lblNgayBatDau, lblNgayKetThuc;
 	private JTextField txtTimNV, txtTimKH;
-	private JButton btnTim, btnLoc, btnLamMoi2, btnLamMoi, btnThoat, btnXemCT;
+	private JButton btnTim, btnLamMoi, btnThoat, btnXemCT;
 	private JDateChooser dateBD, dateKT;
 
 	public HoaDon() {
 		// Khai báo
 		Icon img_out = new ImageIcon("src/img/out16.png");
 		Icon img_search = new ImageIcon("src/img/search.png");
-		Icon img_check = new ImageIcon("src/img/check.png");
 		Icon img_refresh = new ImageIcon("src/img/refresh16.png");
 		Icon img_detail = new ImageIcon("src/img/detail16.png");
-		Box b, bNV, bLoc, bLoc1, bLoc2, bTim, bTim1, bTim2, bChiTiet;
+		Box b, bTim, bTim1, bTim2;
 		Border line = BorderFactory.createLineBorder(Color.BLACK);
 		Dimension dimension = new Dimension(110, 25);
 
-		// Bố cục
+		//NORT
 		b = Box.createHorizontalBox();
-		b.add(Box.createHorizontalStrut(50));
-		b.add(bLoc = Box.createVerticalBox());
-		b.add(Box.createHorizontalStrut(100));
-		b.add(bTim = Box.createVerticalBox());
-		b.add(Box.createHorizontalStrut(100));
-		b.add(bChiTiet = Box.createVerticalBox());
-		b.add(Box.createHorizontalStrut(100));
-		bLoc.add(bLoc1 = Box.createHorizontalBox());
-		bLoc.add(Box.createVerticalStrut(5));
-		bLoc.add(bLoc2 = Box.createHorizontalBox());
+		b.add(bTim = Box.createVerticalBox());		
 		bTim.add(bTim1 = Box.createHorizontalBox());
-		bTim.add(Box.createVerticalStrut(5));
-		bTim.add(bTim2 = Box.createHorizontalBox());
-
-		// Lọc
-		bLoc.setBorder(BorderFactory.createTitledBorder(line, "Lọc theo ngày"));
-		bLoc1.add(lblNgayBatDau = new JLabel("Ngày bắt đầu"));
-		bLoc1.add(Box.createHorizontalStrut(10));
-		bLoc1.add(dateBD = new JDateChooser());
-		bLoc1.add(Box.createHorizontalStrut(10));
-		bLoc1.add(btnLoc = new JButton("Lọc", img_search));
-		bLoc2.add(lblNgayKetThuc = new JLabel("Ngày kết thúc"));
-		bLoc2.add(Box.createHorizontalStrut(10));
-		bLoc2.add(dateKT = new JDateChooser());
-		bLoc2.add(Box.createHorizontalStrut(10));
-		bLoc2.add(btnLamMoi = new JButton("Làm mới", img_refresh));
-
-		// Tra cứu
 		bTim.setBorder(BorderFactory.createTitledBorder(line, "Tra cứu"));
+		bTim1.add(Box.createHorizontalStrut(10));
+		bTim1.add(lblNgayBatDau = new JLabel("Ngày bắt đầu"));
+		bTim1.add(Box.createHorizontalStrut(15));
+		bTim1.add(dateBD = new JDateChooser());
+		bTim1.add(Box.createHorizontalStrut(50));
 		bTim1.add(lblTenNV = new JLabel("Tên nhân viên"));
-		bTim1.add(Box.createHorizontalStrut(10));
-		bTim1.add(txtTimNV = new JTextField(15));
-		bTim1.add(Box.createHorizontalStrut(10));
-		bTim1.add(btnTim = new JButton("Tìm", img_search));
+		bTim1.add(Box.createHorizontalStrut(15));
+		bTim1.add(txtTimNV = new JTextField(10));
+		bTim1.add(Box.createHorizontalStrut(50));
+		bTim1.add(btnTim = new ButtonGradient("Tìm", img_search));
+		bTim1.add(Box.createHorizontalStrut(5));
+		
+		bTim.add(Box.createVerticalStrut(5));
+		bTim.add(bTim2 = Box.createHorizontalBox());		
+		bTim2.add(Box.createHorizontalStrut(10));
+		bTim2.add(lblNgayKetThuc = new JLabel("Ngày kết thúc"));
+		bTim2.add(Box.createHorizontalStrut(15));
+		bTim2.add(dateKT = new JDateChooser());
+		bTim2.add(Box.createHorizontalStrut(50));
 		bTim2.add(lblSđtKH = new JLabel("Số điện thoại khách"));
-		bTim2.add(Box.createHorizontalStrut(10));
-		bTim2.add(txtTimKH = new JTextField(15));
-		bTim2.add(Box.createHorizontalStrut(10));
-		bTim2.add(btnLamMoi2 = new JButton("Làm mới", img_refresh));
+		bTim2.add(Box.createHorizontalStrut(15));
+		bTim2.add(txtTimKH = new JTextField(10));
+		bTim2.add(Box.createHorizontalStrut(50));
+		bTim2.add(btnLamMoi = new ButtonGradient("Làm mới", img_refresh));
+		bTim2.add(Box.createHorizontalStrut(5));
 
-		//
-		bChiTiet.setBorder(BorderFactory.createTitledBorder(" "));
-		bChiTiet.add(btnThoat = new JButton("Thoát", img_out));
-		bChiTiet.add(Box.createVerticalStrut(5));
-		bChiTiet.add(btnXemCT = new JButton("Xem chi tiết", img_detail));
+		b.add(Box.createHorizontalStrut(50));
+		b.add(btnXemCT = new ButtonGradient("Xem chi tiết", img_detail));
+		b.add(Box.createHorizontalStrut(50));
+		b.add(btnThoat = new ButtonGradient("Thoát", img_out));
+		b.add(Box.createHorizontalStrut(50));
 
-		// setSize button
-		btnLoc.setPreferredSize(dimension);
-		btnLamMoi.setPreferredSize(dimension);
-		btnTim.setPreferredSize(dimension);
-		btnLamMoi2.setPreferredSize(dimension);
-
+		btnTim.setPreferredSize(btnLamMoi.getMaximumSize());
 		lblNgayBatDau.setPreferredSize(lblNgayKetThuc.getPreferredSize());
 		lblTenNV.setPreferredSize(lblSđtKH.getPreferredSize());
 
+		//CENTER
 		tableModel = new DefaultTableModel(headers, 0);
 		JScrollPane scroll = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
@@ -105,8 +90,11 @@ public class HoaDon extends JPanel {
 		table.setRowHeight(50);
 		table.setAutoCreateRowSorter(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		
+		//Add giao diện
 		this.setLayout(new BorderLayout());
 		this.add(b, BorderLayout.NORTH);
+//		this.setBackground(Color.decode("#cccccc"));
 		this.add(scroll, BorderLayout.CENTER);
 	}
 }
