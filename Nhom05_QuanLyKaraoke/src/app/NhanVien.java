5f7df926d0bd1a5f7c0d4d3c6add72aa9c45c848
nguyentranhaidang <dangff421@gmail.com>
2023-11-04 16:33:03 +0700

    Update_NhanVien

--- Nhom05_QuanLyKaraoke/src/app/NhanVien.java
+++ Nhom05_QuanLyKaraoke/src/app/NhanVien.java
@@ -15,9 +15,11 @@ import java.awt.event.ActionListener;
 public class NhanVien extends JPanel {
 
 	private JLabel lblTenNhanVien, lblNamSinh, lblGioiTinh, lblSoDienThoai, lblCCCD, lblChucVu, lblMatKhau, lblTinhTrang;
-	private JTextField txtTenNhanVien, txtNamSinh, txtSoDienThoai, txtCCCD, txtMatKhau;
-	private JButton btnThemMoi, btnCapNhat, btnXoa, btnLamMoi, btnThoat, btnTraCuu;
+	private JTextField txtTenNhanVien, txtNamSinh, txtSoDienThoai, txtCCCD, txtMatKhau, txtTimNhanVien, txtTimSDT;
+	private JButton btnThemMoi, btnCapNhat, btnXoa, btnLamMoi, btnThoat, btnTim;
 	private JComboBox cbChucVu, cbTinhTrang;
+	private String[] headers = { "Mã nhân viên", "Tên nhân viên", "Năm sinh", "Giới tính", "Số điện thoại", 
+			"CCCD", "Chức vụ", "Mật khẩu"};
 	private JRadioButton radNam, radNu;
 	private JDateChooser dateNamSinh;
 	private JTable table;
@@ -36,7 +38,7 @@ public class NhanVien extends JPanel {
 
 		Box bLeft = Box.createVerticalBox();
 		Box bRight = Box.createVerticalBox();
-		Box b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17;
+		Box b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b92, b;
 		Dimension dimension = new Dimension(170, 25);
 
 		Border line = BorderFactory.createLineBorder(Color.BLACK);
@@ -89,7 +91,7 @@ public class NhanVien extends JPanel {
 		cbChucVu.addItem("Phục vụ");
 
 		bLeft.add(b7 = Box.createHorizontalBox());
-		bLeft.add(Box.createVerticalStrut(15));
+		bLeft.add(Box.createVerticalStrut(40));
 		b7.add(lblMatKhau = new JLabel("Mật khẩu"));
 		b7.add(Box.createHorizontalStrut(20));
 		b7.add(txtMatKhau = new JTextField());
@@ -104,131 +106,61 @@ public class NhanVien extends JPanel {
 		lblMatKhau.setPreferredSize(lblTenNhanVien.getPreferredSize());
 		
 		
-		bLeft.add(b8 = Box.createHorizontalBox());
-		b8.add(Box.createHorizontalStrut(5));
-		b8.add(btnThemMoi = new JButton("Thêm mới", img_add));
-		btnThemMoi.setBorder(new RoundedBorder(7));
-		btnThemMoi.setBackground(Color.decode("#6fa8dc"));
-		btnThemMoi.setMaximumSize(new Dimension(Integer.MAX_VALUE, btnThemMoi.getMinimumSize().height));
-		bLeft.add(Box.createVerticalStrut(5));
-		
-		b8.add(Box.createHorizontalStrut(20));
-		b8.add(btnCapNhat = new JButton("Cập nhật", img_edit));
-		btnCapNhat.setBorder(new RoundedBorder(7));
-		btnCapNhat.setBackground(Color.decode("#6fa8dc"));
-		btnCapNhat.setMaximumSize(new Dimension(Integer.MAX_VALUE, btnCapNhat.getMinimumSize().height));
-		bLeft.add(Box.createVerticalStrut(5));
-
-		bLeft.add(b9 = Box.createHorizontalBox());
-		b9.add(Box.createVerticalStrut(70));
-		b9.add(Box.createHorizontalStrut(5));
-		b9.add(btnXoa = new JButton("      Xóa      ", img_del));
-		btnXoa.setBorder(new RoundedBorder(7));
-		btnXoa.setBackground(Color.decode("#6fa8dc"));
-		btnXoa.setMaximumSize(new Dimension(Integer.MAX_VALUE, btnXoa.getMinimumSize().height));
-		bLeft.add(Box.createVerticalStrut(5));
-		
-		b9.add(Box.createHorizontalStrut(25));
-		b9.add(btnLamMoi = new JButton("Làm mới", img_reset));
-		btnLamMoi.setBorder(new RoundedBorder(7));
-		btnLamMoi.setBackground(Color.decode("#6fa8dc"));
-		btnLamMoi.setMaximumSize(new Dimension(Integer.MAX_VALUE, btnLamMoi.getMinimumSize().height));
-		bLeft.add(Box.createVerticalStrut(5));
-
-		bLeft.add(b10 = Box.createHorizontalBox());
-		b10.add(Box.createVerticalStrut(50));
-		b10.add(Box.createHorizontalStrut(7));
-		b10.add(btnThoat = new JButton("Thoát", img_out));
-		btnThoat.setBorder(new RoundedBorder(5));
-		btnThoat.setBackground(Color.decode("#6fa8dc"));
+		JPanel pnlKM = new JPanel();
+		GridLayout gridKM = new GridLayout(2, 2);
+		pnlKM.setLayout(gridKM);
+		gridKM.setHgap(30);
+		gridKM.setVgap(10);
+		pnlKM.add(btnThemMoi = new ButtonGradient("Thêm mới", img_add));
+		pnlKM.add(btnCapNhat = new ButtonGradient("Cập nhật", img_edit));
+		pnlKM.add(btnXoa = new ButtonGradient("Xóa", img_del));
+		pnlKM.add(btnLamMoi = new ButtonGradient("Làm mới", img_reset));
+		bLeft.add(pnlKM);
+		bLeft.add(Box.createVerticalStrut(20));
+		bLeft.add(b92 = Box.createHorizontalBox());
+		b92.add(Box.createVerticalStrut(20));
+		b92.add(Box.createHorizontalStrut(3));
+		b92.add(btnThoat = new ButtonGradient("Thoát", img_out));		
 		btnThoat.setMaximumSize(new Dimension(Integer.MAX_VALUE, btnThoat.getMinimumSize().height));
-		bLeft.add(Box.createVerticalStrut(300));
 
-
-		Box aNorth = Box.createHorizontalBox(), aCenter = Box.createVerticalBox();
-		Box a, a1, a2;
-
-		aNorth.add(a = Box.createHorizontalBox());
-		aNorth.setMaximumSize(new Dimension(Integer.MAX_VALUE, aNorth.getMinimumSize().height));
-		aNorth.add(Box.createHorizontalStrut(200));
-
-		aNorth.add(a1 = Box.createVerticalBox());
-		
-		aNorth.add(Box.createVerticalStrut(20));
-		a1.add(b11 = Box.createHorizontalBox());
-		
-		b11.add(Box.createVerticalStrut(20));
-		b11.add(lblChucVu = new JLabel("Chức vụ"));
-		b11.add(Box.createHorizontalStrut(10));
-		b11.add(cbChucVu = new JComboBox<>());
-		a1.add(Box.createVerticalStrut(10));
-		a1.setMaximumSize(new Dimension(Integer.MAX_VALUE, a1.getMinimumSize().height));
-		lblChucVu.setPreferredSize(lblTenNhanVien.getPreferredSize());
-		
-		
-		a1.add(b12 = Box.createHorizontalBox());
-		b12.add(Box.createVerticalStrut(20));
-		b12.add(lblTinhTrang = new JLabel("Tình trạng"));
-		b12.add(Box.createHorizontalStrut(10));
-		b12.add(cbTinhTrang = new JComboBox<>());
-		a1.add(Box.createVerticalStrut(10));
-		a1.setMaximumSize(new Dimension(Integer.MAX_VALUE, a1.getMinimumSize().height));
-		lblTinhTrang.setPreferredSize(lblTenNhanVien.getPreferredSize());
-		a1.add(Box.createVerticalStrut(22));
-		
-		
-		aNorth.add(Box.createHorizontalStrut(300));
-		
-		
-        aNorth.add(a2 = Box.createVerticalBox());
-		
-		a2.setBorder(BorderFactory.createTitledBorder(line, "Tra cứu", TitledBorder.LEFT, TitledBorder.TOP, new Font("Arial", Font.PLAIN, 13), Color.BLACK));
-		aNorth.add(Box.createVerticalStrut(30));
-		a2.add(b12 = Box.createHorizontalBox());
-		
-		b12.add(Box.createVerticalStrut(20));
-		b12.add(lblTenNhanVien = new JLabel("Tên nhân viên"));
-		b12.add(Box.createHorizontalStrut(20));
-		b12.add(txtTenNhanVien = new JTextField());
-		b12.add(Box.createHorizontalStrut(30));
-		b12.add(btnTraCuu = new JButton("  Tìm  ", img_search));
-		btnTraCuu.setBorder(new RoundedBorder(1));
-	    btnTraCuu.setBackground(Color.decode("#6fa8dc"));
-		a2.add(Box.createVerticalStrut(22));
-		//lblTenNhanVien.setPreferredSize(lblTenNhanVien.getPreferredSize());
-		
-		a2.add(b13 = Box.createHorizontalBox());
-		b13.add(Box.createVerticalStrut(20));
-		b13.add(lblSoDienThoai = new JLabel("Số điện thoại"));
-		b13.add(Box.createHorizontalStrut(20));
-		b13.add(txtSoDienThoai = new JTextField());
-		b13.add(Box.createHorizontalStrut(30));
-		b13.add(btnTraCuu = new JButton("Làm mới", img_reset));
-		btnTraCuu.setBorder(new RoundedBorder(1));
-	    btnTraCuu.setBackground(Color.decode("#6fa8dc"));
-	    a2.setMaximumSize(new Dimension(Integer.MAX_VALUE, a2.getMinimumSize().height));
-	    lblSoDienThoai.setPreferredSize(lblTenNhanVien.getPreferredSize());
-	    aNorth.add(Box.createHorizontalStrut(150));
 
 	
-		
-	    bRight.add(aNorth);
-		aCenter.setBorder(BorderFactory.createTitledBorder(line, "Danh sách nhân viên", TitledBorder.LEFT, TitledBorder.TOP, new Font("Arial", Font.PLAIN, 13), Color.BLACK));
-		String[] headers = "Mã nhân viên;Tên nhân viên;Năm sinh;Giới tính;Số điện thoại;CCCD;Chức vụ;Mật khẩu"
-				.split(";");
+		bLeft.add(Box.createVerticalStrut(200));
+
+
+		//b = Box.createHorizontalBox();
+		//b.add(Box.createVerticalStrut(50));
+		JPanel pnlTraCuu = new JPanel();
+		GridLayout gridTraCuu = new GridLayout(2, 5);
+		gridTraCuu.setHgap(10);
+		gridTraCuu.setVgap(5);
+		pnlTraCuu.setLayout(gridTraCuu);
+		pnlTraCuu.setBorder(BorderFactory.createTitledBorder(line, "Tra cứu"));
+		pnlTraCuu.add(lblChucVu = new JLabel("Chức vụ:"));
+		pnlTraCuu.add(cbChucVu = new JComboBox<>());
+		pnlTraCuu.add(lblTenNhanVien = new JLabel("Tên nhân viên:"));
+		pnlTraCuu.add(txtTenNhanVien = new JTextField(10));
+		pnlTraCuu.add(btnTim = new ButtonGradient("Tìm", img_search));
+		pnlTraCuu.add(lblTinhTrang = new JLabel("Tình trạng:"));
+		pnlTraCuu.add(cbTinhTrang = new JComboBox<>());
+		pnlTraCuu.add(lblSoDienThoai = new JLabel("Số điện thoại:"));
+		pnlTraCuu.add(txtSoDienThoai = new JTextField(10));
+		pnlTraCuu.add(btnLamMoi = new ButtonGradient("Làm mới", img_reset));
+		bRight.add(pnlTraCuu);
+	
 		tableModel = new DefaultTableModel(headers, 0);
-		JScrollPane scroll = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+		JScrollPane scroll = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scroll.setBorder(BorderFactory.createTitledBorder(line, "Danh sách nhân viên"));
 		scroll.setViewportView(table = new JTable(tableModel));
-		table.setRowHeight(25);
+		table.setRowHeight(50);
 		table.setAutoCreateRowSorter(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-		aCenter.add(scroll);
-		bRight.add(aCenter);
+		bRight.add(scroll);
 
 		
-		pnlLeft.setBackground(Color.decode("#cccccc"));
-		pnlRight.setBackground(Color.decode("#e6dbd1"));
+		//pnlLeft.setBackground(Color.decode("#cccccc"));
+		//pnlRight.setBackground(Color.decode("#e6dbd1"));
 		pnlLeft.add(bLeft, BorderLayout.WEST);
 		pnlRight.add(bRight, BorderLayout.CENTER);
 
