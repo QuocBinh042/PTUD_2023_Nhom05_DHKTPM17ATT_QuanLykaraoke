dd67ddccdcfaa853c561e6d629d8ff3cd64bfd03
DucNhat <cuem8h@gmail.com>
2023-11-12 00:40:44 +0700

    update DAOLoaiPhong

--- Nhom05_QuanLyKaraoke/src/dao/daoLoaiPhong.java
+++ Nhom05_QuanLyKaraoke/src/dao/daoLoaiPhong.java
@@ -26,4 +26,29 @@ public class daoLoaiPhong {
 		}
 		return dsLoaiPhong;
 	}
+	public ArrayList<LoaiPhong> getCBSC(String sucChua) {
+		ArrayList<LoaiPhong> dsLoaiPhong = new ArrayList<LoaiPhong>();
+		ConnectDB.getInstance();
+		Connection connect = ConnectDB.getConnection();
+		String sql = "";
+		if(sucChua.equalsIgnoreCase("Tất cả")) {
+			sql = "select *from LoaiPhong";
+		}else if(sucChua.equalsIgnoreCase("10")) {
+			sql = "select *from LoaiPhong where SucChua = 10";
+		}else if(sucChua.equalsIgnoreCase("15")) {
+			sql = "select *from LoaiPhong where SucChua = 15";
+		}else if(sucChua.equalsIgnoreCase("20")) {
+			sql = "select *from LoaiPhong where SucChua = 20";
+		}
+		try {
+			Statement statement = connect.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+				dsLoaiPhong.add(new LoaiPhong(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDouble(4)));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dsLoaiPhong;
+	}
 }
