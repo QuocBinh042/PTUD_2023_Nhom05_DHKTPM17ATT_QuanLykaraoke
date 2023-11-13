27888fda3cafe976c19a703d90b11edcc9511bb0
QuocBinh042 <tranlequocbinh2003@gmail.com>
2023-11-13 15:25:39 +0700

    Update TrangChu

--- Nhom05_QuanLyKaraoke/src/app/TrangChu.java
+++ Nhom05_QuanLyKaraoke/src/app/TrangChu.java
@@ -1,7 +1,22 @@
 package app;
 
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 
-public class TrangChu extends JPanel{
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
 
+public class TrangChu extends JPanel {
+	public TrangChu() {
+		ImageIcon icon = new ImageIcon("src/img/trangChu.jpg");
+		Image image = icon.getImage();
+		JLabel label = new JLabel(icon);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Image scaledImage = image.getScaledInstance(screenSize.width, screenSize.height, Image.SCALE_SMOOTH);
+		label.setIcon(new ImageIcon(scaledImage));
+		add(label);
+
+	}
 }
