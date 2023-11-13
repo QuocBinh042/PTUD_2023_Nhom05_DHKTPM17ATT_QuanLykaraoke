ef15269cd39469c2931043358651e2300271584d
nguyentranhaidang <dangff421@gmail.com>
2023-11-14 01:03:15 +0700

    Add Gui KhachHang

--- Nhom05_QuanLyKaraoke/src/app/KhachHang.java
+++ Nhom05_QuanLyKaraoke/src/app/KhachHang.java
@@ -21,16 +21,15 @@ import dao.daoKhachHang;
 import dao.MaTuDong;
 import dao.daoDichVu;
 
-
 public class KhachHang extends JPanel implements MouseListener {
 
-	private JLabel lblTenKhachHang, lblGioiTinh, lblSoDienThoai, lblEmail,lblGhiChu, lblLoaiKhachHang;
+	private JLabel lblTenKhachHang, lblGioiTinh, lblSoDienThoai, lblEmail, lblGhiChu, lblLoaiKhachHang;
 	private JTextField txtTenKhachHang, txtSoDienThoai, txtEmail, txtTimSDT, txtTimTenKH;
 	private JTextArea txaGhiChu;
-	private JButton btnThemMoi, btnCapNhat,  btnLamMoi, btnThoat, btnTim, btnLamMoiKH;
+	private JButton btnThemMoi, btnCapNhat, btnLamMoi, btnThoat, btnTim, btnLamMoiKH;
 	private JComboBox cbLoaiKhachHang, cbGioiTinh;
-	private String[] headers = { "Mã khách hàng", "Tên khách hàng", "Loại khách hàng", "Giới tính", "Số điện thoại", "Email",
-			"Số giờ đã thuê", "Ghi chú"};
+	private String[] headers = { "Mã khách hàng", "Tên khách hàng", "Loại khách hàng", "Giới tính", "Số điện thoại",
+			"Email", "Số giờ đã thuê", "Ghi chú" };
 	private JTable table;
 	private DefaultTableModel tableModel;
 	private daoKhachHang daoKH = new daoKhachHang();
@@ -38,18 +37,15 @@ public class KhachHang extends JPanel implements MouseListener {
 	private MaTuDong maKhachHang = new MaTuDong();
 	private Boolean gt;
 
-
-
-
 	public KhachHang() {
-		
+
 		try {
 			ConnectDB.getInstance().connect();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		daoKH = new daoKhachHang();
-		
+
 		Icon img_add = new ImageIcon("src/img/add2.png");
 		Icon img_del = new ImageIcon("src/img/del.png");
 		Icon img_reset = new ImageIcon("src/img/refresh.png");
@@ -62,8 +58,7 @@ public class KhachHang extends JPanel implements MouseListener {
 
 		Box bLeft = Box.createVerticalBox();
 		Box bRight = Box.createVerticalBox();
-		Box b1, b2, b3, b4, b5, bLoc, bTacVu, bTim, bTim1,
-				bTim2, bLoc1, bLoc2;
+		Box b1, b2, b3, b4, b5, bLoc, bTacVu, bTim, bTim1, bTim2, bLoc1, bLoc2;
 		Dimension dimension = new Dimension(170, 25);
 		Dimension dimension2 = new Dimension(120, 20);
 		Border line = BorderFactory.createLineBorder(Color.BLACK);
@@ -81,7 +76,7 @@ public class KhachHang extends JPanel implements MouseListener {
 		b2.add(cbGioiTinh = new JComboBox<>());
 		cbGioiTinh.addItem("Nam");
 		cbGioiTinh.addItem("Nữ");
-	
+
 		bLeft.add(b3 = Box.createHorizontalBox());
 		bLeft.add(Box.createVerticalStrut(15));
 		b3.add(lblSoDienThoai = new JLabel("Số điện thoại"));
@@ -100,13 +95,13 @@ public class KhachHang extends JPanel implements MouseListener {
 		b5.add(Box.createHorizontalStrut(20));
 		b5.add(txaGhiChu = new JTextArea(5, 15));
 		txaGhiChu.setBorder(line);
-		
+
 		lblTenKhachHang.setPreferredSize(lblTenKhachHang.getPreferredSize());
 		lblSoDienThoai.setPreferredSize(lblTenKhachHang.getPreferredSize());
 		lblGioiTinh.setPreferredSize(lblTenKhachHang.getPreferredSize());
 		lblEmail.setPreferredSize(lblTenKhachHang.getPreferredSize());
 		lblGhiChu.setPreferredSize(lblTenKhachHang.getPreferredSize());
-		
+
 		JPanel pnlKH = new JPanel();
 		GridLayout gridKH = new GridLayout(2, 2);
 		pnlKH.setLayout(gridKH);
@@ -119,42 +114,41 @@ public class KhachHang extends JPanel implements MouseListener {
 		pnlKH.setBackground(Color.decode("#cccccc"));
 		bLeft.add(pnlKH);
 		bLeft.add(Box.createVerticalStrut(340));
-		
-		//Loc
+
+		// Loc
 		bRight.add(bTacVu = Box.createHorizontalBox());
 		bTacVu.add(Box.createHorizontalStrut(100));
-		
+
 		bLoc = Box.createVerticalBox();
-		JPanel pnlLblLoaiKhachHang = new JPanel();	
+		JPanel pnlLblLoaiKhachHang = new JPanel();
 		JPanel pnlCbLoaiKhachHang = new JPanel();
 		pnlLblLoaiKhachHang.setBackground((Color.decode("#e6dbd1")));
 		pnlCbLoaiKhachHang.setBackground((Color.decode("#e6dbd1")));
-		
-		
+
 		pnlLblLoaiKhachHang.add(lblLoaiKhachHang = new JLabel("Loại khách hàng:"));
 		pnlCbLoaiKhachHang.add(cbLoaiKhachHang = new JComboBox<>());
 		cbLoaiKhachHang.addItem("Tất cả");
 		cbLoaiKhachHang.addItem("Thường");
 		cbLoaiKhachHang.addItem("VIP");
-		
+
 		bLoc.add(bLoc1 = Box.createHorizontalBox());
 		bLoc1.add(pnlLblLoaiKhachHang);
 		bLoc1.add(pnlCbLoaiKhachHang);
-		
+
 		JPanel pnlLoc = new JPanel();
 		pnlLoc.add(bLoc);
-	    pnlLoc.setBackground((Color.decode("#e6dbd1")));
-		//pnlLoc.setBorder(BorderFactory.createTitledBorder(line, "Lọc"));
+		pnlLoc.setBackground((Color.decode("#e6dbd1")));
+		// pnlLoc.setBorder(BorderFactory.createTitledBorder(line, "Lọc"));
 		bTacVu.add(pnlLoc);
-		
+
 		bTacVu.add(Box.createHorizontalStrut(100));
-		
-		//Tim
+
+		// Tim
 		bTim = Box.createVerticalBox();
 		JPanel pnlLblTenKhachHang = new JPanel();
 		JPanel pnlTxtTenKhachHang = new JPanel();
 		JPanel pnlLblSoDienThoai = new JPanel();
-		JPanel pnlTxtSoDienThoai = new JPanel();		
+		JPanel pnlTxtSoDienThoai = new JPanel();
 		pnlLblTenKhachHang.setBackground((Color.decode("#cccccc")));
 		pnlLblSoDienThoai.setBackground((Color.decode("#cccccc")));
 		pnlTxtTenKhachHang.setBackground((Color.decode("#cccccc")));
@@ -163,7 +157,7 @@ public class KhachHang extends JPanel implements MouseListener {
 		pnlTxtTenKhachHang.add(txtTimTenKH = new JTextField(10));
 		pnlLblSoDienThoai.add(lblSoDienThoai = new JLabel("Số điện thoại:"));
 		pnlTxtSoDienThoai.add(txtTimSDT = new JTextField(10));
-		
+
 		bTim.add(bTim1 = Box.createHorizontalBox());
 		bTim1.add(pnlLblTenKhachHang);
 		bTim1.add(pnlTxtTenKhachHang);
@@ -179,9 +173,9 @@ public class KhachHang extends JPanel implements MouseListener {
 		pnlTim.setBackground((Color.decode("#cccccc")));
 		pnlTim.setBorder(BorderFactory.createTitledBorder(line, "Tra cứu"));
 		bTacVu.add(pnlTim);
-		
+
 		bTacVu.add(Box.createHorizontalStrut(100));
-		
+
 		//
 		lblSoDienThoai.setPreferredSize(lblTenKhachHang.getPreferredSize());
 		cbLoaiKhachHang.setPreferredSize(dimension);
@@ -200,7 +194,7 @@ public class KhachHang extends JPanel implements MouseListener {
 		table.setAutoCreateRowSorter(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		bRight.add(scroll);
-		
+
 		//
 		pnlLeft.setBackground(Color.decode("#cccccc"));
 		pnlRight.setBackground(Color.decode("#e6dbd1"));
@@ -212,106 +206,135 @@ public class KhachHang extends JPanel implements MouseListener {
 		this.setLayout(new BorderLayout());
 		add(pnlLeft, BorderLayout.WEST);
 		add(pnlRight, BorderLayout.CENTER);
-		
-		layToanBoKH();
+
+		dsKH = daoKH.getAll();
+		layDanhSachKH(dsKH);
+
 		btnLamMoiKH.addActionListener(e -> xuLyLamMoi());
 		btnLamMoi.addActionListener(e -> xuLyTraCuu());
 		btnThemMoi.addActionListener(e -> xuLyThemMoi());
 		btnCapNhat.addActionListener(e -> xuLyCapNhat());
 		btnThoat.addActionListener(e -> System.exit(0));
 		btnTim.addActionListener(e -> xuLyTimKiem());
+		cbLoaiKhachHang.addActionListener(e -> xuLyCBLoaiKH());
 		table.addMouseListener((MouseListener) this);
-		
-		//String[] row = { "KH001", "tenKH", "", "", "sdt", "email", "5", ""};
-		//tableModel.addRow(row); 
 	}
-	
-	
-	
-	// Kiem tra rang buoc
-		private boolean validData() {
-			String tenKH = txtTenKhachHang.getText();
-			String sdt = txtSoDienThoai.getText();
-			String email = txtEmail.getText();
-			
-			Pattern p = Pattern.compile("[a-zA-Z]+");
-			if (!(p.matcher(tenKH).find())) {
-				JOptionPane.showMessageDialog(null, "Tên khách hàng không hợp lệ!");
-				return false;
-			}
-			
-			Pattern p1 = Pattern.compile("[0-9]{10}");
-			if (!(p1.matcher(sdt).find())) {
-				JOptionPane.showMessageDialog(null, "Số điện thoại chỉ được nhập chữ số!");
-				return false;
-			}
-			
-			Pattern p2 = Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-					+ "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
-			if (!(p2.matcher(email).find())) {
-				JOptionPane.showMessageDialog(null, "Email không được để trống!");
-				return false;
-			}
-			
-			
-			return true;
+
+	// Xoa toan bo dich vu
+	private void xoaToanBoKH() {
+		DefaultTableModel dm = (DefaultTableModel) table.getModel();
+		while (dm.getRowCount() > 0) {
+			dm.removeRow(0);
 		}
-	
+
+	}
+
+	// Lay toan bo khach hang
+	private void layDanhSachKH(ArrayList<entity.KhachHang> dsKH) {
+		xoaToanBoKH();
+		for (entity.KhachHang kh : dsKH) {
+			String gioiTinh = kh.getGioiTinh() ? "Nữ" : "Nam";
+			String loaikh = kh.getLoaiKH() ? "VIP" : "Thường";
+			tableModel.addRow(new Object[] { kh.getMaKH(), kh.getTenKH(), loaikh, gioiTinh, kh.getSdthoai(),
+					kh.getEmail(), kh.getSoGioDaThue(), kh.getGhiChu() });
+
+		}
+	}
+
+	// Xu ly combobox loai khach hang
+	private void xuLyCBLoaiKH() {
+		Boolean loaiKH;
+		if (cbLoaiKhachHang.getSelectedItem().toString().equalsIgnoreCase("VIP")) {
+			loaiKH = true;
+		}
+		else loaiKH = false;
+		
+		dsKH = daoKhachHang.getKhachHangCB(loaiKH);
+		layDanhSachKH(dsKH);
+
+	}
+
+	// Kiem tra rang buoc
+	private boolean validData() {
+		String tenKH = txtTenKhachHang.getText();
+		String sdt = txtSoDienThoai.getText();
+		String email = txtEmail.getText();
+
+		Pattern p = Pattern.compile("[a-zA-Z]+");
+		if (!(p.matcher(tenKH).find())) {
+			JOptionPane.showMessageDialog(null, "Tên khách hàng không hợp lệ!");
+			return false;
+		}
+
+		Pattern p1 = Pattern.compile("[0-9]{10}");
+		if (!(p1.matcher(sdt).find())) {
+			JOptionPane.showMessageDialog(null, "Số điện thoại chỉ được nhập chữ số!");
+			return false;
+		}
+
+		Pattern p2 = Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+				+ "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
+		if (!(p2.matcher(email).find())) {
+			JOptionPane.showMessageDialog(null, "Email không được để trống!");
+			return false;
+		}
+
+		return true;
+	}
+
 	// Xu ly them moi
 	private void xuLyThemMoi() {
 		int luaChon = JOptionPane.showConfirmDialog(null, "Có chắc chắn thêm thông tin khách hàng không?", "Chú ý",
 				JOptionPane.YES_NO_OPTION);
 		if (luaChon == JOptionPane.YES_OPTION) {
 			if (validData() == true) {
-			String tenKH = txtTenKhachHang.getText();
-			String gioiTinh = cbGioiTinh.getSelectedItem().toString();
-			if (gioiTinh  == "Nam") {
-				 gt = false;
-			}else 
-                 gt = true;
-			String sdt = txtSoDienThoai.getText();
-			String email = txtEmail.getText();
-			String ghichu = txaGhiChu.getText();
-		    String maKH = maKhachHang.formatMa(dsKH.get(dsKH.size()-1).getMaKH());
-			String[] row = { maKH , tenKH, "", gioiTinh, sdt, email, "", ghichu };
-            dsKH.add(new entity.KhachHang(maKH, tenKH, false, false, sdt, email, 0, ghichu));
-			tableModel.addRow(row);
-			JOptionPane.showMessageDialog(null, "Thêm mới khách hàng thành công!");
-		}
-			
-		}
-		else {
+				String tenKH = txtTenKhachHang.getText();
+				String gioiTinh = cbGioiTinh.getSelectedItem().toString();
+				if (gioiTinh == "Nam") {
+					gt = false;
+				} else
+					gt = true;
+				String sdt = txtSoDienThoai.getText();
+				String email = txtEmail.getText();
+				String ghichu = txaGhiChu.getText();
+				String maKH = maKhachHang.formatMa(dsKH.get(dsKH.size() - 1).getMaKH());
+				String[] row = { maKH, tenKH, "VIP", gioiTinh, sdt, email, "3", ghichu };
+				dsKH.add(new entity.KhachHang(maKH, tenKH, false, false, sdt, email, 0, ghichu));
+				tableModel.addRow(row);
+				JOptionPane.showMessageDialog(null, "Thêm mới khách hàng thành công!");
+			}
+
+		} else {
 			JOptionPane.showMessageDialog(null, "Vui lòng nhập đầy đủ thông tin khách hàng!");
 		}
 	}
-	
+
 	private Object xuLyCapNhat() {
 		// TODO Auto-generated method stub
 		int luaChon = JOptionPane.showConfirmDialog(null, "Có chắc chắn muốn cập nhật thông tin khách hàng không?",
 				"Chú ý", JOptionPane.YES_NO_OPTION);
 		if (luaChon == JOptionPane.YES_OPTION) {
 			if (validData() == true) {
-			String tenKH = txtTenKhachHang.getText();
-			String gioiTinh = cbGioiTinh.getSelectedItem().toString();
-			String sdt = txtSoDienThoai.getText();
-			String email = txtEmail.getText();
-			String ghichu = txaGhiChu.getText();
-			int viTri = table.getSelectedRow();
-			String maKH = (String) table.getValueAt(table.getSelectedRow(),0);
-			tableModel.removeRow(viTri);
-			String[] row = { maKH , tenKH, "", gioiTinh, sdt, email, "", ghichu };
-		    daoKH.updateKhachHang(new entity.KhachHang(maKH, tenKH, false, false, sdt, email, 0, ghichu));
-			tableModel.insertRow(viTri, row);
-			JOptionPane.showMessageDialog(null, "Cập nhật khách hàng thành công!");
-		}else {
-			JOptionPane.showMessageDialog(null, "Vui lòng nhập đầy đủ thông tin khách hàng!");
-		}
+				String tenKH = txtTenKhachHang.getText();
+				String gioiTinh = cbGioiTinh.getSelectedItem().toString();
+				String sdt = txtSoDienThoai.getText();
+				String email = txtEmail.getText();
+				String ghichu = txaGhiChu.getText();
+				int viTri = table.getSelectedRow();
+				String maKH = (String) table.getValueAt(table.getSelectedRow(), 0);
+				tableModel.removeRow(viTri);
+				String[] row = { maKH, tenKH, "Thường", gioiTinh, sdt, email, "2", ghichu };
+				daoKH.updateKhachHang(new entity.KhachHang(maKH, tenKH, false, false, sdt, email, 0, ghichu));
+				tableModel.insertRow(viTri, row);
+				JOptionPane.showMessageDialog(null, "Cập nhật khách hàng thành công!");
+			} else {
+				JOptionPane.showMessageDialog(null, "Vui lòng nhập đầy đủ thông tin khách hàng!");
+			}
 		}
 		return null;
-		
+
 	}
-	
-	
+
 	private Object xuLyLamMoi() {
 		// TODO Auto-generated method stub
 		txtTenKhachHang.setText("");
@@ -321,16 +344,14 @@ public class KhachHang extends JPanel implements MouseListener {
 		txaGhiChu.setText("");
 		return null;
 	}
-	
 
 	private Object xuLyTraCuu() {
 		// TODO Auto-generated method stub
-		txtTimTenKH.setText("");		
+		txtTimTenKH.setText("");
 		txtTimSDT.setText("");
 		return null;
 	}
-	
-	
+
 	private void xuLyTimKiem() {
 		String tenKHTim = txtTimTenKH.getText();
 		int n = 0;
@@ -346,68 +367,40 @@ public class KhachHang extends JPanel implements MouseListener {
 			JOptionPane.showMessageDialog(null, "Tên khách hàng không tồn tại!");
 		}
 	}
-	
-	
-	
-	
-	// Xoa toan bo dich vu
-		private void xoaToanBoKH() {
-			DefaultTableModel dm = (DefaultTableModel) table.getModel();
-			while (dm.getRowCount() > 0) {
-				dm.removeRow(0);
-			}
 
-		}
-
-	
-	// Lay toan bo khach hang
-		private void layToanBoKH() {
-			dsKH = daoKH.getAll();
-			for (entity.KhachHang kh : dsKH) {
-			  String gioiTinh = kh.getGioiTinh() ? "Nữ" : "Nam";
-			  String loaikh = kh.getLoaiKH() ? "VIP" : "Thường";
-				tableModel.addRow(new Object[] { kh.getMaKH(), kh.getTenKH(), loaikh, gioiTinh,
-						kh.getSdthoai(), kh.getEmail(), kh.getSoGioDaThue(), kh.getGhiChu()});
-
-			}
-		}
-
-	
-	
 	// Xu ly mouseclick
-	    @Override
-		public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-		}
+	}
 
-	    @Override
-		public void mousePressed(MouseEvent e) {
-			int row = table.getSelectedRow();
-			txtTenKhachHang.setText(table.getValueAt(row, 1).toString());
-			cbGioiTinh.setSelectedItem(table.getValueAt(row, 3).toString());
-			txtSoDienThoai.setText(table.getValueAt(row, 4).toString());
-			txtEmail.setText(table.getValueAt(row, 5).toString());
-			txaGhiChu.setText(table.getValueAt(row, 7).toString());
-		}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int row = table.getSelectedRow();
+		txtTenKhachHang.setText(table.getValueAt(row, 1).toString());
+		cbGioiTinh.setSelectedItem(table.getValueAt(row, 3).toString());
+		txtSoDienThoai.setText(table.getValueAt(row, 4).toString());
+		txtEmail.setText(table.getValueAt(row, 5).toString());
+		txaGhiChu.setText(table.getValueAt(row, 7).toString());
+	}
 
-	    @Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-		}
-	    @Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
+	}
 
-		}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-	    @Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
+	}
 
-		}
-	
-	
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
