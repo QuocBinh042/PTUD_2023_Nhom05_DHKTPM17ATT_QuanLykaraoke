695f61504adaca8ae65e5fadd08a2042daa1627f
nguyentranhaidang <dangff421@gmail.com>
2023-11-14 01:00:21 +0700

    Add daoKhachHang.java

--- Nhom05_QuanLyKaraoke/src/dao/daoKhachHang.java
+++ Nhom05_QuanLyKaraoke/src/dao/daoKhachHang.java
@@ -8,22 +8,22 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import entity.KhachHang;
 import connectDB.ConnectDB;
 
-public class daoKhachHang{
+public class daoKhachHang {
 
 	public ArrayList<KhachHang> getAll() {
-		// TODO Auto-generated method stubArrayList<HoaDonDichVuPhong> dsDVP = new ArrayList<HoaDonDichVuPhong>();
+		// TODO Auto-generated method stubArrayList<HoaDonDichVuPhong> dsDVP = new
+		// ArrayList<HoaDonDichVuPhong>();
 		ArrayList<KhachHang> dsKH = new ArrayList<KhachHang>();
 		ConnectDB.getInstance();
 		Connection con = ConnectDB.getConnection();
 		try {
 			String sql = "select * from KhachHang";
-			Statement statement =con.createStatement();
+			Statement statement = con.createStatement();
 			ResultSet rs = statement.executeQuery(sql);
-			while(rs.next()) {
+			while (rs.next()) {
 				dsKH.add(new KhachHang(rs.getString(1), rs.getString(2), rs.getBoolean(3), rs.getBoolean(4),
 						rs.getString(5), rs.getString(6), rs.getInt(7), rs.getString(8)));
 			}
@@ -33,8 +33,33 @@ public class daoKhachHang{
 		}
 		return dsKH;
 	}
-		
-	
+
+	// Lay khach hang theo combobox
+	public static ArrayList<KhachHang> getKhachHangCB(Boolean loaiKH) {
+		ArrayList<KhachHang> dsKH = new ArrayList<KhachHang>();
+		ConnectDB.getInstance();
+		Connection connect = ConnectDB.getConnection();
+		String sql = "";
+		if (loaiKH) {
+			sql = "SELECT * FROM KhachHang WHERE loaiKH = ?";
+		} else if (!loaiKH) {
+			sql = "SELECT * FROM KhachHang WHERE loaiKH = ?";
+		}
+		try {
+			PreparedStatement preparedStatement = connect.prepareStatement(sql);
+			preparedStatement.setBoolean(1, loaiKH);
+
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				dsKH.add(new KhachHang(rs.getString(1), rs.getString(2), rs.getBoolean(3), rs.getBoolean(4),
+						rs.getString(5), rs.getString(6), rs.getInt(7), rs.getString(8)));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dsKH;
+	}
+
 	public boolean add(KhachHang kh) {
 		// TODO Auto-generated method stub
 		ConnectDB.getInstance();
@@ -58,8 +83,7 @@ public class daoKhachHang{
 			// TODO: handle exception
 			e.printStackTrace();
 			return false;
-		}
-		finally {
+		} finally {
 			close(stm);
 		}
 		return true;
@@ -73,7 +97,7 @@ public class daoKhachHang{
 		String sql = "Update KhachHang set TenKH = ?, LoaiKH = ?, GioiTinh = ?, SoDienThoai = ?, Email = ?, SoGioDaThue = ?, GhiChu = ?\r\n"
 				+ "where MaKH = ?";
 		try {
-			stm = con.prepareStatement(sql);		
+			stm = con.prepareStatement(sql);
 			stm.setString(1, kh.getTenKH());
 			stm.setBoolean(2, kh.getLoaiKH());
 			stm.setBoolean(3, kh.getGioiTinh());
@@ -82,19 +106,17 @@ public class daoKhachHang{
 			stm.setInt(6, kh.getSoGioDaThue());
 			stm.setString(7, kh.getGhiChu());
 			stm.setString(8, kh.getMaKH());
-			
+
 			stm.executeUpdate();
 		} catch (SQLException e) {
 			// TODO: handle exception
 			e.printStackTrace();
 			return false;
-		}
-		finally {
+		} finally {
 			close(stm);
 		}
 		return true;
 	}
-	
 
 	public void delete(String tenKH) {
 		// TODO Auto-generated method stub
@@ -105,15 +127,15 @@ public class daoKhachHang{
 		try {
 			stm = con.prepareStatement(sql);
 			stm.setString(1, tenKH);
-			
+
 			stm.executeUpdate();
 		} catch (SQLException e) {
 			// TODO: handle exception
 		}
 	}
-	 
+
 	public void close(PreparedStatement stm) {
-		if(stm!=null) {
+		if (stm != null) {
 			try {
 				stm.close();
 			} catch (SQLException e) {
@@ -122,5 +144,5 @@ public class daoKhachHang{
 			}
 		}
 	}
-	
+
 }
