9331941c79e268ec8b6233e9a2562792febd2cfc
QuocBinh042 <tranlequocbinh2003@gmail.com>
2023-12-01 21:22:57 +0700

    Update Card

--- Nhom05_QuanLyKaraoke/src/app/Card.java
+++ Nhom05_QuanLyKaraoke/src/app/Card.java
@@ -1,34 +1,15 @@
 package app;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Cursor;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Arrays;
 import java.util.List;
+import javax.swing.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.border.Border;
-
-public class Card extends JFrame{
+public class Card extends JFrame {
 	private JButton btnTrangChu, btnPhongHat, btnPhong, btnDichVu, btnKhachHang, btnNhanVien, btnHoaDon, btnKhuyenMai,
-	btnThongKe, btnTroGiup, btnDangXuat;
+			btnThongKe, btnTroGiup, btnDangXuat;
 	private JPanel pnl = new JPanel(new CardLayout());
 	private TrangChu tc = new TrangChu();
 	private DatPhong dp = new DatPhong();
@@ -41,22 +22,69 @@ public class Card extends JFrame{
 	private ThongKe tk = new ThongKe();
 	private TroGiup tg = new TroGiup();
 	private JPanel pnlButton = new JPanel();
-	
-	
+
 	public Card() {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		createUI();
+		addEventListeners();
+	}
+
+	private void createUI() {
+		// Set frame properties
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1400, 720);
 		setLocationRelativeTo(null);
-		this.setLayout(new BorderLayout());
-		JPanel pane = new JPanel(new BorderLayout());
-		JPanel pnlLeft = new JPanel(new BorderLayout());
-		JPanel pnlLogo = new JPanel();
-		ImageIcon icon = new ImageIcon("src/img/One.png");
-		JLabel label = new JLabel(icon);
-		pnlLogo.add(Box.createVerticalStrut(50));
-		pnlLogo.add(label);		
-		pnlLogo.setBackground(Color.decode("#990447"));
-		pnlButton.setBackground(Color.decode("#990447"));
+		setLayout(new BorderLayout());
+
+		// Create components
+		btnTrangChu = createButton(" TRANG CHỦ ", "src/img/home.png");
+		btnPhongHat = createButton(" PHÒNG HÁT ", "src/img/karaoke.png");
+		btnPhong =    createButton(" PHÒNG          ", "src/img/room.png");
+		btnDichVu =   createButton(" DỊCH VỤ         ", "src/img/service.png");
+		btnKhachHang =createButton(" KHÁCH HÀNG", "src/img/client.png");
+		btnNhanVien = createButton(" NHÂN VIÊN ", "src/img/staff.png");
+		btnHoaDon =   createButton(" HÓA ĐƠN   ", "src/img/invoice.png");
+		btnKhuyenMai =createButton(" KHUYẾN MÃI", "src/img/discount.png");
+		btnThongKe =  createButton(" THỐNG KÊ  ", "src/img/statistical.png");
+		btnTroGiup =  createButton(" TRỢ GIÚP  ", "src/img/help.png");
+		btnDangXuat = createButton(" ĐĂNG XUẤT ", "src/img/out.png");
+		createPanelLayout();
+
+		// Add components to the frame
+		add(createLeftPanel(), BorderLayout.WEST);
+		add(pnl, BorderLayout.CENTER);
+	}
+
+	private JButton createButton(String text, String iconPath) {
+		ImageIcon icon = new ImageIcon(iconPath);
+		JButton button = new JButton(text, icon);
+		button.setFont(new Font("Arial", Font.BOLD, 12));
+//		button.setHorizontalAlignment(SwingConstants.LEFT);
+		button.setBackground(Color.decode("#e6dbd1"));
+		button.setForeground(Color.BLACK);
+		button.setFocusPainted(false);
+		button.setBorderPainted(false);
+		button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		button.setPreferredSize(new Dimension(180, 40));
+		pnlButton.add(button);
+		return button;
+	}
+
+	private void createPanelLayout() {
 		pnl.add(tc, "tc");
 		pnl.add(dp, "dp");
 		pnl.add(dv, "dv");
@@ -68,349 +96,49 @@ public class Card extends JFrame{
 		pnl.add(tk, "tk");
 		pnl.add(tg, "tg");
 		pnl.add(tg, "dx");
-		
-        addButton(btnTrangChu = new JButton("TRANG CHỦ"));
-        addButton(btnPhongHat = new JButton("PHÒNG HÁT"));
-        addButton(btnPhong = new JButton("PHÒNG"));
-        addButton(btnDichVu = new JButton("DỊCH VỤ"));
-        addButton(btnKhachHang = new JButton("KHÁCH HÀNG"));
-        addButton(btnNhanVien = new JButton("NHÂN VIÊN"));
-        addButton(btnHoaDon = new JButton("HÓA ĐƠN"));
-        addButton(btnKhuyenMai = new JButton("KHUYẾN MÃI"));
-        addButton(btnThongKe = new JButton("THỐNG KÊ DOANH THU"));
-        addButton(btnTroGiup = new JButton("TRỢ GIÚP"));
-        addButton(btnDangXuat = new JButton("ĐĂNG XUẤT"));
-        
-		pnlButton.setPreferredSize(new Dimension(200, 720));		
-		btnTrangChu.addMouseListener(new MouseListener() {			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnTrangChu.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnTrangChu.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		btnPhongHat.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnPhongHat.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnPhongHat.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		btnPhong.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnPhong.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnPhong.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		btnDichVu.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnDichVu.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnDichVu.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		btnHoaDon.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnHoaDon.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnHoaDon.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-			
-		btnNhanVien.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnNhanVien.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnNhanVien.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		btnKhachHang.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnKhachHang.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnKhachHang.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		btnThongKe.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnThongKe.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnThongKe.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-	
-		btnTroGiup.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnTroGiup.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnTroGiup.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		btnKhuyenMai.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnKhuyenMai.setBackground(Color.decode("#e6dbd1"));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				btnKhuyenMai.setBackground(Color.decode("#B5B5B5"));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-	
-		
-		
-		pnlLeft.add(pnlLogo, BorderLayout.NORTH);
+	}
+
+	private JPanel createLeftPanel() {
+		JPanel pnlLeft = new JPanel(new BorderLayout());
+		pnlLeft.add(createLogoPanel(), BorderLayout.NORTH);
 		pnlLeft.add(pnlButton, BorderLayout.CENTER);
 		pnlLeft.setBorder(BorderFactory.createLineBorder(Color.black));
-		
-		this.add(pnlLeft, BorderLayout.WEST);
-		this.add(pnl, BorderLayout.CENTER);
-		
+		pnlButton.setPreferredSize(new Dimension(200, 700));
+		return pnlLeft;
+	}
+
+	private JPanel createLogoPanel() {
+		JPanel pnlLogo = new JPanel();
+		ImageIcon icon = new ImageIcon("src/img/Logo.png");
+		JLabel label = new JLabel(icon);
+		pnlLogo.add(label);
+		pnlLogo.setBackground(Color.decode("#990447"));
+		pnlButton.setBackground(Color.decode("#990447"));
+		return pnlLogo;
+	}
+
+	private void addEventListeners() {
+		List<JButton> buttons = Arrays.asList(btnTrangChu, btnPhongHat, btnPhong, btnDichVu, btnKhachHang, btnNhanVien,
+				btnHoaDon, btnKhuyenMai, btnThongKe, btnTroGiup, btnDangXuat);
+		buttons.forEach(button -> button.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				button.setBackground(Color.decode("#e6dbd1"));
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				button.setBackground(Color.decode("#B5B5B5"));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		}));
 		btnPhong.addActionListener(e -> xuLyPhong());
 		btnPhongHat.addActionListener(e -> xuLyPhongHat());
 		btnKhachHang.addActionListener(e -> xuLyKhachHang());
@@ -421,51 +149,29 @@ public class Card extends JFrame{
 		btnThongKe.addActionListener(e -> xuLyThongKe());
 		btnTroGiup.addActionListener(e -> xuLyTroGiup());
 		btnTrangChu.addActionListener(e -> xuLyTrangChu());
-		btnDangXuat.addActionListener(e ->{
-			int i = JOptionPane.showConfirmDialog(null, "Bạn có chắc chắn muốn đăng xuất khỏi hệ thống không?", "Chú ý",
-					JOptionPane.YES_NO_OPTION);
-			if (i == JOptionPane.YES_OPTION) {
-				dispose();
-				new app.Login().setVisible(true);
-			}
-			else {
-				
-			}
-		});
+		btnDangXuat.addActionListener(e -> xuLyDangXuat());
 	}
-	
-	private void addButton(JButton button) {
-		button.setAlignmentX(Component.LEFT_ALIGNMENT);
-	    button.setBackground(Color.decode("#e6dbd1"));
-	    button.setForeground(Color.BLACK);
-	    button.setFocusPainted(false);
-	    button.setBorderPainted(false);
-	    button.setCursor(new Cursor(Cursor.HAND_CURSOR));
-	    button.setPreferredSize(new Dimension(180, 40));
-//	    pnlButton.add(Box.createVerticalStrut(20));
-	    pnlButton.add(button);
-    }
 
 	private void xuLyPhong() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "phong");
 	}
-	
+
 	private void xuLyPhongHat() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "dp");
 	}
-	
+
 	private void xuLyDichVu() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "dv");
 	}
-	
+
 	private void xuLyNhanVien() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "nv");
 	}
-	
+
 	private void xuLyKhachHang() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "kh");
@@ -475,24 +181,37 @@ public class Card extends JFrame{
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "hd");
 	}
-	
+
 	private void xuLyThongKe() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "tk");
 	}
-	
+
 	private void xuLyTroGiup() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "tg");
 	}
-	
+
 	private void xuLyKhuyenMai() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "km");
 	}
-	
+
 	private void xuLyTrangChu() {
 		CardLayout card = (CardLayout) pnl.getLayout();
 		card.show(pnl, "tc");
+	}
+
+	private void xuLyDangXuat() {
+		int result = JOptionPane.showConfirmDialog(null, "Bạn có chắc chắn muốn đăng xuất khỏi hệ thống không?",
+				"Chú ý", JOptionPane.YES_NO_OPTION);
+		if (result == JOptionPane.YES_OPTION) {
+			dispose();
+			new app.Login().setVisible(true);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Card().setVisible(true);
 	}
 }
